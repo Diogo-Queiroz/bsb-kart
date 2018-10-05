1c1339d2a92fd50f2f2f7cfcb382686cf46d6834
DESKTOP-V06510B\diogo <diogoqueirooz1212@gmail.com>
2018-10-04 23:36:45 -0300

    Inserido o link para usar o bootstrap, alterado o css para usar a fonte Montserrat, criado o formulario de cadastro e o link.

--- src/component/navigation.js
+++ src/component/navigation.js
@@ -9,8 +9,8 @@ const Navigation = () =>
     <li><Link to={routes.ACCOUNT}>Account</Link></li>
     <li><Link to={routes.LANDING}>Landing</Link></li>
     <li><Link to={routes.PASS_FORGET}>Forget Password</Link></li>
-    <li><Link to={routes.SIGN_IN}>Sign Up</Link></li>
-    <li><Link to={routes.SIGN_UP}>Sign In</Link></li>
+    <li><Link to={routes.SIGN_UP}>Sign Up</Link></li>
+    <li><Link to={routes.SIGN_IN}>Sign In</Link></li>
   </ul>
 
 export default Navigation
--- src/component/signup.js
+++ src/component/signup.js
@@ -1,8 +1,73 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import * as routes from '../constants/routes'
 
 const SignUpPage = () =>
-  <div>
+  <div className='container'>
     <h1>SignUp Page</h1>
+    <div className='row'>
+      <div className='col'></div>
+      <div className='col-5'>
+        <SignUpForm />
+      </div>
+      <div className='col'></div>
+    </div>
   </div>
 
+class SignUpForm extends Component {
+  constructor (props) {
+    super(props)
+  }
+
+  submitForm = (e) => {
+    e.preventDefault()
+    console.log('form submitted')
+  }
+
+  render () {
+    return (
+      <form onSubmit={this.submitForm}>
+        <div className='form-group'>
+          <label htmlFor='username'>Full Name:</label>
+          <input type='text' id='username'
+            className='form-control' placeholder='Full Name'/>
+        </div>
+        <div className='form-group'>
+          <label htmlFor='email'>E-mail:</label>
+          <input type='email' id='email'
+            className='form-control' placeholder='email' />
+        </div>
+
+        <div className='form-group'>
+          <label htmlFor='passwordOne'>Password:</label>
+          <input type='password' id='passwordOne'
+            className='form-control' placeholder='Password' />
+        </div>
+
+        <div className='form-group'>
+          <label htmlFor='passwordTwo'>Confirm Password:</label>
+          <input type='password' id='passwordTwo'
+            className='form-control' placeholder='Password' />
+        </div>
+
+        <div className='form-group'>
+          <div className='row justify-content-md-around'>
+            <button className='btn btn-primary btn-lg col-4' type='submit'>Sign Up</button>
+            <button className='btn btn-danger btn-lg col-4' type='reset'>Reset</button>
+          </div>
+        </div>
+      </form>
+    )}
+}
+
+const SignUpLink = () =>
+  <p>
+    Don't have an account??
+    <Link to={routes.SIGN_UP}>Sign Up</Link>
+  </p>
+
 export default SignUpPage
+
+export {
+  SignUpForm, SignUpLink
+}
