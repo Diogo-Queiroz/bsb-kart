b248f491b955f516cf7e798bdb6d69a98d0e7e4c
DESKTOP-V06510B\diogo <diogoqueirooz1212@gmail.com>
2018-10-07 23:53:01 -0300

    Finished the signin form, next step is create the sign out method.

--- src/component/signin.js
+++ src/component/signin.js
@@ -1,8 +1,102 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { withRouter } from 'react-router-dom'
 
-const SignInPage = () =>
-  <div>
+import { SignUpLink } from './signup'
+import { auth } from '../firebase'
+import * as routes from '../constants/routes'
+
+const SignInPage = ({ history }) =>
+  <div className='container'>
     <h1>SignIn Page</h1>
+    <div className='row'>
+      <div className='col'></div>
+      <div className='col-sm-12 col-md-8 col-lg-10'>
+        <SignInForm history={history} />
+        <SignUpLink />
+      </div>
+    </div>
   </div>
 
-export default SignInPage
+const byPropKey = (propertyName, value) => () => ({
+  [propertyName]: value
+})
+
+const INITIAL_STATE = {
+  email: '',
+  password: '',
+  error: null
+}
+
+class SignInForm extends Component {
+  constructor (props) {
+    super (props)
+
+    this.state = {...INITIAL_STATE}
+  }
+
+  submitForm = (e) => {
+    const {
+      email, password
+    } = this.state
+    const {
+      history
+    } = this.props
+
+    auth.doSignInWithEmailAndPassword(email, password)
+      .then(() => {
+        this.setState({...INITIAL_STATE})
+        history.push(routes.HOME)
+      })
+      .catch(error => {
+        this.setState(byPropKey('error', error))
+      })
+
+    e.preventDefault()
+  }
+
+  render () {
+    const {
+      email, password, error
+    } = this.state
+
+    const isInvalid = password === '' || email === ''
+
+    return (
+      <form onSubmit={this.submitForm}>
+
+        <div className='form-group'>
+          <label htmlFor='email'>E-mail:</label>
+          <input type='email' id='email'
+            className='form-control' placeholder='email'
+            value={email} onChange={event => this.setState(byPropKey('email',
+              event.target.value
+            ))} />
+        </div>
+
+        <div className='form-group'>
+          <label htmlFor='password'>Password:</label>
+          <input type='password' id='password'
+            className='form-control' placeholder='Password'
+            value={password} onChange={event => this.setState(byPropKey('password',
+              event.target.value
+            ))} />
+        </div>
+
+        <div className='form-group'>
+          <div className='row justify-content-md-around'>
+            <button 
+              className='btn btn-primary btn-lg col'
+              type='submit' disabled={isInvalid}>Sign Up
+            </button>
+            <button className='btn btn-danger btn-lg col' type='reset'>Reset</button>
+          </div>
+        </div>
+        { error && <p>{ error.message }</p>}
+      </form>
+    )
+  }
+}
+
+export default withRouter(SignInPage)
+
+export { SignInForm }
